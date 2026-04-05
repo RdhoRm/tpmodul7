7bd2b6b4d912b71164ad57e425d9628413b8aafb
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-04-06 06:12:45 +0700

    menambahkan class KuliahMahasiswa103082400026

--- tpmodul7_103082400026/Program.cs
+++ tpmodul7_103082400026/Program.cs
@@ -6,8 +6,15 @@ namespace tpmodul7_103082400026
     {
         static void Main(string[] args)
         {
+            // --- Pemanggilan Bagian 1 ---
             DataMahasiswa103082400026 data = new DataMahasiswa103082400026();
             data.ReadJSON();
+
+            Console.WriteLine(); // Memberi jarak kosong (Enter)
+
+            // --- Pemanggilan Bagian 2 ---
+            KuliahMahasiswa103082400026 kuliah = new KuliahMahasiswa103082400026();
+            kuliah.ReadJSON();
 
             Console.ReadLine(); // Menahan layar terminal
         }
