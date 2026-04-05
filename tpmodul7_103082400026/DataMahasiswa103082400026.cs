368aac657005915fdaa5095a34ef4147e5d7911e
Ridho Ramadhan <ridho.ramadan1016@gmail.com>
2026-04-06 06:09:08 +0700

    menambahkan class DataMahasiswa103082400026

---
+++ tpmodul7_103082400026/DataMahasiswa103082400026.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace tpmodul7_103082400026
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public class DataMahasiswa103082400026
+    {
+        public void ReadJSON()
+        {
+            // Membaca file JSON
+            string jsonString = File.ReadAllText("tp7_1_103082400026.json");
+
+            // Proses deserialization
+            Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+            // Mencetak hasil
+            Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+        }
+    }
+}
--- tpmodul7_103082400026/Program.cs
+++ tpmodul7_103082400026/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace tpmodul7_103082400026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMahasiswa103082400026 data = new DataMahasiswa103082400026();
+            data.ReadJSON();
+
+            Console.ReadLine(); // Menahan layar terminal
+        }
+    }
+}
